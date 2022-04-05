0e5aa4ae0ae265134ed431c55992785b761e7f84
kingmingseo <multi197122@gmail.com>
2022-04-05 12:37:22 +0900

    2022_04_04-R

--- R통계분석/R시각화 점_선.R
+++ R통계분석/R시각화 점_선.R
@@ -1,3 +1,16 @@
 x=c(1:10)
 dotchart(x,label=paste("test",1:10),pch=22)
-par(mai=c(1,1,1,1))
+
+
+x=c(1,2,3,4,5,6,7,8,9)
+y=x*2
+z=x*3
+plot(x,y,type="o",ylim=c(0,30))
+points(x,z,pch="+")
+lines(x,z,col="blue")
+  dev.off()
+
+  
+b=c(1,2,1,4,3,5,4,5,3,2,5,6,7,2,8,5,9,5,3,5)
+hist(b)
+  
--- R통계분석/R시각화.R
+++ R통계분석/R시각화.R
@@ -1,14 +1,27 @@
-par("mar")
+x= seq(1,10,0.1)
+y=exp(x)
+plot(x,y,type="l",col="blue",lty=2)
+exp(3)
+title("Exponential value")
+dev.off()
+data(ToothGrowth)
+head(ToothGrowth)
+dim(ToothGrowth)
+plot(ToothGrowth)
 
-par(mar=c(1,1,1,1))
+
+dev.off()
+B_QTY=c(110,300,150,280,310)
+S_QTY=c(180,200,210,190,170)
+P_QTY=c(210,150,260,210,70)
+B_TYPE=matrix(c(B_QTY,S_QTY,P_QTY),5,3)
+B_TYPE
+barplot((B_TYPE), main="Ball TYPE별 시즌 판매량",xlab="Ball TYPE",ylab="판매량",beside=T,names.arg=c("BASEBALL","SOCCERBALL","PINGPONGBALL"),border="blue",col=rainbow(5),ylim=c(0,400))
+
+legend(1,400,c("Season A","Season B","Season C","Season D","Season E"),cex=0.5, fill = rainbow(5))
 
 
-plot(10:1)
-close.screen(all=TRUE)
-split.screen(c(2,1))
-split.screen(c(1,3),screen=2)
-screen(1)
-plot(10:1)
-screen(4)
-plot(1:10)
-rm(list=ls())
+barplot(t(B_TYPE), main="시즌별 시즌 판매량",xlab="SEASON",ylab="판매량",beside=F,names.arg=c("A","B","C","D","E"),border="black",col=rainbow(3),ylim=c(0,1000))
+
+legend(4.5,1000,c("BaseBall","Soccer Ball","PingpongBall"),cex=0.8, fill = rainbow(3))
+
